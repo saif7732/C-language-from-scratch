a6daa2ec81ec19ae9b1e5b24b3ea5825b2801ec3
Md. Saiful Haque Saif <saifulhaquesaifpc@gmail.com>
2025-08-11 00:52:43 +0600

    added

--- 07_forloop.c
+++ 07_forloop.c
@@ -4,11 +4,11 @@ int main(){
 
     int n;
     printf("enter the value of n:");
-    //scanf("%d", &n);
+    scanf("%d", &n);
 
-    for ( int i = 0; 1; i++){
+    for ( int i = 0; i<n; i++){
 
-        printf("the value of %d is %d\n", i*i, i*i);
+        printf("%d. Hello Mr.\n", i);
        
     }
 
